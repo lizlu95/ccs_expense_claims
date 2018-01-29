15faf18609e3859f293b49711f5e498a20ff2dee
Carl Johnston <carljohnston28@gmail.com>
2018-01-28 20:16:20 -0800

    Use login specific Pug view

--- routes/authenticate/login.js
+++ routes/authenticate/login.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 // GET /login
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Login' });
+  res.render('authenticate/login', { title: 'Login' });
 });
 
 module.exports = router;
