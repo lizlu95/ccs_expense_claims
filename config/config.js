295af6fab1ebdc771b7f6011c6a38f6973b68e6d
Carl Johnston <carljohnston28@gmail.com>
2018-01-30 13:54:24 -0800

    Switch to sequelize for database migrations

--- config/config.js
+++ config/config.js
@@ -3,22 +3,25 @@ const config = {};
 config.database = {
   test: {
     host: process.env.ECA_DATABASE_HOST_TEST || 'localhost',
-    user: process.env.ECA_DATABASE_USER_TEST || 'username',
-    pass: process.env.ECA_DATABASE_PASS_TEST || 'password',
-    name: process.env.ECA_DATABASE_NAME_TEST || '',
+    username: process.env.ECA_DATABASE_USERNAME_TEST || 'username',
+    password: process.env.ECA_DATABASE_PASSWORD_TEST || 'password',
+    database: process.env.ECA_DATABASE_NAME_TEST || 'database_test',
+    dialect: 'mysql',
   },
   development: {
     host: process.env.ECA_DATABASE_HOST_DEVELOPMENT || 'localhost',
-    user: process.env.ECA_DATABASE_USER_DEVELOPMENT || 'username',
-    pass: process.env.ECA_DATABASE_PASS_DEVELOPMENT || 'password',
-    name: process.env.ECA_DATABASE_NAME_DEVELOPMENT || '',
+    username: process.env.ECA_DATABASE_USERNAME_DEVELOPMENT || 'username',
+    password: process.env.ECA_DATABASE_PASSWORD_DEVELOPMENT || 'password',
+    database: process.env.ECA_DATABASE_NAME_DEVELOPMENT || 'database_development',
+    dialect: 'mysql',
   },
-  production: {
+  development: {
     host: process.env.ECA_DATABASE_HOST_PRODUCTION || 'localhost',
-    user: process.env.ECA_DATABASE_USER_PRODUCTION || 'username',
-    pass: process.env.ECA_DATABASE_PASS_PRODUCTION || 'password',
-    name: process.env.ECA_DATABASE_NAME_PRODUCTION || '',
-  }
-};
+    username: process.env.ECA_DATABASE_USERNAME_PRODUCTION || 'username',
+    password: process.env.ECA_DATABASE_PASSWORD_PRODUCTION || 'password',
+    database: process.env.ECA_DATABASE_NAME_PRODUCTION || 'database_prod',
+    dialect: 'mysql',
+  },
+}
 
 module.exports = config;
