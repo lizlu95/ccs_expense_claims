2cc80207943b63f246809f1714f8d50ab4fd7cea
Carl Johnston <carljohnston28@gmail.com>
2018-01-30 22:34:14 -0800

    Add belongsTo relationship from Employee to Manager

--- models/Employee.js
+++ models/Employee.js
@@ -2,18 +2,35 @@
 
 module.exports = (sequelize, DataTypes) => {
   var Employee = sequelize.define('Employee', {
-    employeeId: DataTypes.INTEGER,
-    managerId: DataTypes.INTEGER,
+    employeeId: {
+      type: DataTypes.INTEGER,
+      field: 'employee_id',
+    },
+    managerId: {
+      type: DataTypes.INTEGER,
+      field: 'manager_id',
+    },
     email: DataTypes.STRING,
     password: DataTypes.STRING,
     name: DataTypes.STRING,
-    isAdmin: DataTypes.BOOLEAN
-  }, {
-    classMethods: {
-      associate: function(models) {
-        // associations can be defined here
-      }
-    }
+    isAdmin: {
+      type: DataTypes.BOOLEAN,
+      field: 'is_admin',
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      field: 'created_at',
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      field: 'updated_at',
+    },
   });
+
+  Employee.belongsTo(Employee, {
+    foreign_key: 'manager_id',
+    as: 'manager',
+  });
+
   return Employee;
 };
