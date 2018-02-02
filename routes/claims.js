291108c8884b0bbc8a0093665f419f2bf6a1f5e3
Carl Johnston <carljohnston28@gmail.com>
2018-02-01 17:12:46 -0800

    Basic ExpenseClaim creating/detail routing

--- routes/claims.js
+++ routes/claims.js
@@ -1,9 +1,37 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const models = require('../models/index');
+const ExpenseClaim = models.ExpenseClaim;
 
 /* GET /claims/new */
-router.get('/new', function(req, res, next) {
+router.get('/new', function (req, res, next) {
   res.render('claims/new', { title: 'Expense Claim' });
+});
+
+router.get('/:id', function (req, res, next) {
+  var expenseClaimId = req.params.id;
+  ExpenseClaim.findById(expenseClaimId).then((expenseClaim) => {
+    console.log("BEFORE")
+    console.log(expenseClaimId)
+    if (expenseClaim) {
+      console.log("MADE IT")
+      res.locals.id = expenseClaimId;
+      res.locals.status = expenseClaim.status;
+
+      res.render('claims/detail');
+    } else {
+      next();
+    }
+  });
+});
+
+/* POST /claims */
+router.post('', function (req, res, next) {
+  new ExpenseClaim({
+    status: 'pending',
+  }).then((expenseClaim) => {
+    res.redirect('/claims/' + expenseClaim.id);
+  });
 });
 
 module.exports = router;
