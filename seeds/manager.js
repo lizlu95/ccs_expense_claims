cf1e1c42a326ffdd225a1bc401cf951eadacaa23
Carl Johnston <carljohnston28@gmail.com>
2018-02-15 12:43:02 -0800

    Update db:seed and use eachSeries for seed manager to manage async requests

--- seeds/manager.js
+++ seeds/manager.js
@@ -1,48 +1,68 @@
 const Sequelize = require('sequelize');
 const sequelizeFixtures = require('sequelize-fixtures');
-const models = require('../models/index');
+const database = require('../models/index');
 const _ = require('underscore');
+const async = require('async');
 
 const env = process.env.NODE_ENV || 'development';
 
 const load = function (done) {
-  verifyEnvironment();
+  async.waterfall([
+    function (callback) {
+      var fixturesPath = 'fixtures/' + env + '/*.yml';
+      sequelizeFixtures.loadFile(fixturesPath, database, {
+        log: function () {},
+      }).then(function () {
+        callback(null);
+      });
+    },
+  ], function (err) {
+    if (err) {
+      console.log(err);
 
-  var fixturesPath = 'fixtures/' + env + '/*.yml';
-  sequelizeFixtures.loadFile(fixturesPath, models, {
-      log: function () {},
-    }).then(function () {
+      process.exit();
+    } else {
       if (done) {
         done();
       }
+    }
   });
 };
 
 const destroy = function (done) {
-  verifyEnvironment();
+  async.waterfall([
+    function (callback) {
+      eachModel((model, callback) => {
+        model.destroy({
+          truncate: true,
+        }).then(() => {
+          callback(null);
+        });
+      }, callback);
+    },
+  ], function (err) {
+    if (err) {
+      console.log(err);
 
-  _.each(models, function (model) {
-    if (!(model instanceof Sequelize) && model !== Sequelize) {
-      model.destroy({
-        truncate: true,
-      });
+      process.exit();
+    } else {
+      if (done) {
+        done();
+      }
     }
   });
-
-  if (done) {
-    done();
-  }
 };
 
-function verifyEnvironment() {
-  if (env === 'production') {
-    // only seed production data to a fresh database
-    if (true) {
-      process.exit();
+function eachModel(fn, callback) {
+  async.eachSeries(database.models, (model, callback) => {
+    if (!(model instanceof Sequelize) && model !== Sequelize) {
+      fn(model, callback);
+    } else {
+      callback(null);
     }
-  } else {
-    // test and development environment can re-seed data
-  }
+  }, function (err) {
+    callback(err);
+  });
 }
 
 module.exports = {
