b7ba294fae04e8992cce395b1187214af826769d
Carl Johnston <carljohnston28@gmail.com>
2018-02-18 09:46:26 -0800

    Add basic client side test for new claim Vue model

--- tests/views/new.js
+++ tests/views/new.js
@@ -7,6 +7,7 @@ const CLAIMS_NEW_ROUTE = '/claims/new';
 
 Browser.site = 'http://localhost:9000';
 var browser = new Browser();
+browser.silent = true;
 
 describe('new claims page', () => {
   it('expenseClaimApp component created with initial information and no items', (done) => {
@@ -14,6 +15,15 @@ describe('new claims page', () => {
       function (browser, callback) {
         browser.visit('/claims/new', () => {
           browser.assert.evaluate('expenseClaimApp');
+          browser.assert.evaluate('expenseClaimApp.costCentreNumber === null');
+          browser.assert.evaluate('expenseClaimApp.bankAccount === null');
+          browser.assert.evaluate('expenseClaimApp.items.length === 1');
+          browser.assert.evaluate('expenseClaimApp.items[0].date === null');
+          browser.assert.evaluate('expenseClaimApp.items[0].glId === null');
+          browser.assert.evaluate('expenseClaimApp.items[0].numKm === null');
+          browser.assert.evaluate('expenseClaimApp.items[0].receipt === null');
+          browser.assert.evaluate('expenseClaimApp.items[0].description === null');
+          browser.assert.evaluate('expenseClaimApp.items[0].total === null');
 
           callback(null);
         });
