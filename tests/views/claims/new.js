5ea91b9ee2051d6dca3f04d0ef582d0a27a43256
Carl Johnston <carljohnston28@gmail.com>
2018-02-19 10:46:40 -0800

    Add tests for adding/removing items on new expense claim page

--- tests/views/claims/new.js
+++ tests/views/claims/new.js
@@ -4,6 +4,7 @@ const helper = require('../../helper');
 require('../../../index');
 const async = require('async');
 const manager = require('../../../seeds/manager');
+const _ = require('underscore');
 
 const CLAIMS_NEW_ROUTE = '/claims/new';
 
@@ -40,4 +41,88 @@ describe('new claims page', () => {
       done();
     });
   });
+
+  it('expenseClaimApp item added by clicking on Add Item and removed by clicking -', (done) => {
+    browser.visit('/claims/new', () => {
+      browser.assert.evaluate('expenseClaimApp.items.length === 1');
+
+      var addItemSelector = '#add-item';
+      var removeItemSelector = '#remove-item';
+
+      async.waterfall([
+        (callback) => {
+          var numAddedItems = 2;
+          for (var i = 0; i < numAddedItems; i++) {
+            addExpenseClaimAppItem();
+          }
+          browser.wait().then(() => {
+            var totalItems = numAddedItems + 1;
+            browser.assert.evaluate('expenseClaimApp.items.length === ' + totalItems.toString());
+
+            browser.assert.evaluate('$(".item").length === ' + totalItems.toString());
+
+            callback(null);
+          });
+        },
+        (callback) => {
+          browser.evaluate('expenseClaimApp.items[0].date = 0');
+          browser.evaluate('expenseClaimApp.items[1].date = 1');
+          browser.evaluate('expenseClaimApp.items[2].date = 2');
+
+          browser.evaluate("$('#remove-item').first().click()");
+
+          browser.evaluate('expenseClaimApp.items.length === 2');
+          browser.evaluate('expenseClaimApp.items[0].date === 0');
+          browser.evaluate('expenseClaimApp.items[1].date === 2');
+
+          callback(null);
+        },
+      ], () => {
+        done();
+      });
+    });
+  });
+
+  it('expenseClaimApp computes item total based on sum of item totals', (done) => {
+    browser.visit('/claims/new', () => {
+      browser.assert.evaluate('expenseClaimApp.itemsTotal === 0');
+
+      async.waterfall([
+        (callback) => {
+          var numAddedItems = 2;
+          for (var i = 0; i < numAddedItems; i++) {
+            addExpenseClaimAppItem();
+          }
+
+          browser.wait().then(() => {
+            var totalItems = numAddedItems + 1;
+            browser.assert.evaluate('expenseClaimApp.items.length === ' + totalItems.toString());
+
+            callback(null);
+          });
+        },
+        (callback) => {
+          _.each([[2,3], [-2, -4], [-2,4]], (amounts) => {
+            _.each(amounts, (amount, index) => {
+              browser.evaluate('expenseClaimApp.items[' + index.toString() + '].total = ' + amount.toString());
+            });
+
+            var expectedItemsTotal = _.reduce(amounts, (acc, total) => {
+              return acc + total;
+            });
+
+            browser.assert.evaluate('expenseClaimApp.itemsTotal === ' + expectedItemsTotal.toString());
+          });
+
+          callback(null);
+        },
+      ], () => {
+        done();
+      });
+    });
+  });
 });
+
+function addExpenseClaimAppItem() {
+  browser.evaluate('$("#add-item").click()');
+}
