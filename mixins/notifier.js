c174579d55ef264e9a9491473b2e71e4d7d6f1eb
Carl Johnston <carljohnston28@gmail.com>
2018-02-21 21:48:58 -0800

    Add tests for notifications for sending emails for expense claim creations

--- mixins/notifier.js
+++ mixins/notifier.js
@@ -2,6 +2,7 @@
 
 const nodemailer = require('nodemailer');
 const _ = require('underscore');
+const async = require('async');
 const Promise = require('promise');
 
 const database = require('../models/index');
@@ -9,30 +10,64 @@ const Employee = database.Employee;
 
 class Notifier {
   constructor() {
+    // TODO make this system configurable
+    this.fromEmail = 'no-reply@ccs-expense-claims.herokuapp.com';
+
     var isSecure = process.env.ECA_SMTP_SECURE === 'true';
     this.transporter = nodemailer.createTransport({
-      host: process.env.ECA_SMTP_HOST,
+      host: process.env.ECA_SMTP_HOST || '',
       port: parseInt(process.env.ECA_SMTP_PORT) || isSecure ? 465 : 587,
       secure: isSecure,
       auth: {
-        user: process.env.ECA_SMTP_USERNAME,
-        pass: process.env.ECA_SMTP_PASSWORD,
+        user: process.env.ECA_SMTP_USERNAME || '',
+        pass: process.env.ECA_SMTP_PASSWORD || '',
       }
     });
   }
 
-  notifyCreate(submitterEmail, approverEmail, callback) {
-    var subject = 'Expense Claim Approval Requested';
-    var message = 'Hello, please find the attached link below.';
+  notifyExpenseClaimSubmitted(submitterEmail, approverEmail, callback) {
+    var submitterSubject = 'Expense Claim Approval Submitted';
+    var submitterMessage = 'Hello, please find your submitted request link below.';
+    var approverSubject = 'Expense Claim Approval Requested';
+    var approverMessage = 'Hello, please find the attached link below.';
 
-    return _notify.apply(this, Array.prototype.slice.call(arguments, 0, 2).concat([subject, message, callback]));
+    return new Promise(_.bind((resolve, reject) => {
+      async.series({
+        submitter: (callback) => {
+          _notify.apply(this, [submitterEmail, submitterSubject, submitterMessage])
+            .then((info) => {
+              callback(null);
+            })
+            .then((err) => {
+              callback(null, err);
+            });
+        },
+        approver: (callback) => {
+          _notify.apply(this, [approverEmail, approverSubject, approverMessage])
+            .then((info) => {
+              callback(null);
+            })
+            .then((err) => {
+              callback(null, err);
+            });
+        },
+      }, (err, errs) => {
+        // object of errs for submitter/approver
+        // e.g. { submitter: err, approver: err }
+        resolve(errs);
+
+        if (callback) {
+          callback(errs);
+        }
+      });
+    }, this));
   }
 }
 
-function _notify(submitterEmail, approverEmail, subject, message, callback) {
+function _notify(email, subject, message, callback) {
   let mailOptions = {
-    from: submitterEmail,
-    to: approverEmail,
+    from: this.fromEmail,
+    to: email,
     subject: subject,
     text: message,
   };
@@ -40,12 +75,12 @@ function _notify(submitterEmail, approverEmail, subject, message, callback) {
   return new Promise(_.bind(function (resolve, reject) {
     this.transporter.sendMail(mailOptions, function (err, info) {
       if (err) {
-        reject.apply(this, arguments);
+        reject.apply(this, [err]);
       } else {
-        resolve.apply(this, arguments);
+        resolve.apply(this, [info]);
 
         if (callback) {
-          callback.apply(this, arguments);
+          callback.apply(this, [info]);
         }
       }
     });
--- tests/mixins/notifier.js
+++ tests/mixins/notifier.js
@@ -4,13 +4,17 @@ const expect = chai.expect;
 const app = require('../../app');
 const request = require('supertest');
 const manager = require('../../seeds/manager');
+const sinon = require('sinon');
+const rewire = require('rewire');
+const _ = require('underscore');
+const Promise = require('promise');
 
 const models = require('../../models/index');
 const Employee = models.Employee;
 const ExpenseClaim = models.ExpenseClaim;
 const ExpenseClaimItem = models.ExpenseClaimItem;
 
-const Notifier = require('../../mixins/notifier');
+const Notifier = rewire('../../mixins/notifier');
 
 describe('notifier tests', function () {
   beforeEach(function (done) {
@@ -27,5 +31,43 @@ describe('notifier tests', function () {
     assert(notifier.transporter);
 
     done();
+  });
+
+  it('notifyExpenseClaimSubmitted should send emails to approver and sender', (done) => {
+    var notifier = new Notifier();
+
+    var submitterEmail = 'submitter@submitter.com';
+    var submitterSubject = 'Expense Claim Approval Submitted';
+    var submitterMessage = 'Hello, please find your submitted request link below.';
+
+    var approverEmail = 'approver@approver.com';
+    var approverSubject = 'Expense Claim Approval Requested';
+    var approverMessage = 'Hello, please find the attached link below.';
+
+    var _notifyStub = sinon.stub().returns(Promise.resolve('info'));
+
+    var _notifyOriginal = Notifier.__get__('_notify');
+
+    Notifier.__set__('_notify', _notifyStub);
+
+    var submitterNotifyExpenseClaimSubmittedArgs = [
+      submitterEmail,
+      submitterSubject,
+      submitterMessage,
+    ];
+    var approverNotifyExpenseClaimSubmittedArgs = [
+      approverEmail,
+      approverSubject,
+      approverMessage,
+    ];
+    notifier.notifyExpenseClaimSubmitted(submitterEmail, approverEmail).then(() => {
+      _.each([submitterNotifyExpenseClaimSubmittedArgs, approverNotifyExpenseClaimSubmittedArgs], (args) => {
+        assert.isTrue(_notifyStub.calledWithExactly.apply(_notifyStub, args));
+      });
+
+      Notifier.__set__('_notify', _notifyOriginal);
+
+      done();
+    });
   });
 });
