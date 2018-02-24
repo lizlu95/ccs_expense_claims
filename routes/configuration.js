d62b04697092f32e13a9f1d2d23d2cb7feb6ddd5
Liqiang Yu <waterwavesteven@gmail.com>
2018-02-23 22:32:07 -0800

    show all data

--- routes/configuration.js
+++ routes/configuration.js
@@ -3,71 +3,120 @@ const express = require('express');
 const router = express.Router();
 const _ = require('underscore');
 const models = require('../models/index');
-const Configurations = models.Configuration;
+const Configuration = models.Configuration;
+/* add more if want add more column of table into PUG */
+const SettingKeysOnPug = ['name', 'value'];
 
-/* GET /system/configuration */
+const NAME = 'newSettingsValue';
+
+/* INT(11) = -2147483648_2147483647 */
+const MAXOnDataBase = 2147483647;
+const MINLimit = 0;
+
+/* GET /system/configuration
+   read data from database */
+let settings;
+
 router.get('/', function (req, res, next) {
-  let configurationsData = {};
-  Configurations.findAll().then(function (configurations) {
-    for (let item of configurations) {
-      configurationsData[item.dataValues.name] = item.dataValues.value;
-    }
-    res.render('admin/sysConfig',
-               {
-                 title: 'System Configuration',
-                 max_per_diem_amount_data: configurationsData.max_per_diem_amount,
-                 per_mileage_value_data: configurationsData.per_mileage_value,
-                 max_per_meal_amount_data: configurationsData.max_per_meal_amount
-               });
-  });
-});
-
-/* POST /system/configuration */
-router.post('/', function (req, res, next) {
-  updateValues(req).then(function (nothing) {
-    res.redirect('/system/configuration');
-  });
-
-});
-// Configurations.update() return a promise, should wrap in Promise.all
-updateValues = function (req) {
-  return new Promise(function (fulfill, reject) {
-    let pArr = [];
-    if (req.body.newMaxDiem) {
-      pArr.push(Configurations.update({
-        value: req.body.newMaxDiem,
-      }, {
-        where: {
-          name: 'max_per_diem_amount'
-        }
-      }));
-    }
-    if (req.body.newMileage) {
-      pArr.push(Configurations.update({
-        value: req.body.newMileage,
-      }, {
-        where: {
-          name: 'per_mileage_value'
-        }
-      }));
-    }
-    if (req.body.newMeal) {
-      pArr.push(Configurations.update({
-        value: req.body.newMeal,
-      }, {
-        where: {
-          name: 'max_per_meal_amount'
-        }
-      }));
-    }
-    Promise.all(pArr).then(function (nothing) {
-      fulfill(nothing);
+    getValues().then(function (result) {
+        // console.log(result);
+        res.locals.title = 'System Configuration';
+        res.render('admin/sysConfig',
+            {
+                settingsPug: settings,
+                newSettingValuePug: NAME
+            }
+        )
     }).catch(function (error) {
-      /* TODO when input value is not value, such as too big or negative
-         show the error message or display sth.
-         (node:2160) UnhandledPromiseRejectionWarning: SequelizeDatabaseError: Out of range value for column 'value' at row 1 */
-    });
-  });
+        // console.log(error);
+        res.render('error')
+    })
+});
+
+getValues = function () {
+    getValue = function (item, arr) {
+        let result = {};
+        for (let x of arr) {
+            result[x] = item.dataValues[x];
+        }
+        return result;
+    };
+
+    return new Promise(function (fulfill, reject) {
+        settings = [];
+        Configuration.findAll().then(function (result) {
+            for (let item of result) {
+                settings.push(getValue(item, SettingKeysOnPug))
+            }
+            fulfill({
+                settings: settings
+            });
+        }).catch(function (error) {
+            reject(error)
+        })
+    })
+};
+
+/* POST /system/configuration/settings
+   update database */
+router.post('/settings', function (req, res, next) {
+    updateSettings(req).then(function (result) {
+        // console.log(result);
+        res.redirect('/system/configuration')
+    }).catch(function (error) {
+        // console.log(error);
+        res.redirect('/system/configuration')
+    })
+});
+
+/* updateValue() return a promise, should wrap in Promise.all */
+updateSettings = function (req) {
+    return new Promise(function (fulfill, reject) {
+        let pArr = [];
+        let newValue = req.body[NAME];
+        /* kArr = ['max_per_diem_amount', 'newSettingsValue', 'KM_rates_less_than_5000', 'KM_rates_great_than_5000', 'per_mileage_value', 'max_per_meal_amount'] */
+        let kArr = Object.keys(req.body);
+        let index = kArr.indexOf(NAME);    // <-- Not supported in <IE9
+        if (index !== -1) {
+            kArr.splice(index, 1);
+        }
+        /* kArr = ['max_per_diem_amount', 'KM_rates_less_than_5000', 'KM_rates_great_than_5000', 'per_mileage_value', 'max_per_meal_amount'] */
+        for (let x in kArr) {
+            pArr.push(updateSetting(kArr[x], newValue[x]));
+        }
+        Promise.all(pArr).then(function (result) {
+            fulfill(result)
+        }).catch(function (error) {
+            reject(error)
+        })
+    })
+};
+
+/* update value for one system configuration */
+updateSetting = function (name, value) {
+    let promise;
+    if (value === '') {
+        // no change request for these configuration
+        promise = new Promise(function (fulfill, reject) {
+            fulfill(name + ' does not change')
+        })
+    } else if (value >= MINLimit && value <= MAXOnDataBase) {
+        /*  one change request and input is valid
+            Configurations.update return a promise */
+        promise = Configuration.update({
+            value: value
+        }, {
+            where: {
+                name: name
+            }
+        })
+    } else {
+        /* one change request but input is invalid because over limit */
+        promise = new Promise(function (fulfill, reject) {
+            reject(name + ': ' + value + ' is invalid [' + MINLimit + '<=' + name + '<=' + MAXOnDataBase + ']')
+        })
+    }
+    return promise
 };
 
 module.exports = router;
