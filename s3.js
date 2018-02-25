2b8e95e7ee96bb8e6a1417cfa898ff48d76a18e3
Carl Johnston <carljohnston28@gmail.com>
2018-02-25 11:27:20 -0800

    Remove extra period

--- s3.js
+++ s3.js
@@ -1,5 +1,5 @@
 const S3 = require('aws-sdk/clients/s3');
-const s3 = new S3.({
+const s3 = new S3({
   accessKeyId: process.env.ECA_AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.ECA_AWS_SECRET_ACCESS_KEY,
   region: process.env.ECA_AWS_S3_REGION || 'us-west-2',
