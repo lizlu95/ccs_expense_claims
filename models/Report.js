fa88996247ce72d6368a1122340a8e8458c06281
gebrial <sgebrial@gmail.com>
2018-03-24 19:05:36 -0700

    Implement submitting and saving T24 report

--- models/Report.js
+++ models/Report.js
@@ -1,6 +1,8 @@
 'use strict';
 
 module.exports = (sequelize, DataTypes) => {
+  // TODO: new key column
+
   var Report = sequelize.define('Report', {
     employeeId: {
       type: DataTypes.INTEGER,
@@ -24,6 +26,13 @@ module.exports = (sequelize, DataTypes) => {
 
   Report.associate = function (models) {
     Report.Employee = Report.belongsTo(models.Employee);
+  };
+
+  Report.TYPE = {
+      DEFAULT: 'nav',
+      NAV: 'nav',
+      T24: 't24',
+      PAYROLL: 'payroll',
   };
 
   return Report;
--- routes/admin/reports.js
+++ routes/admin/reports.js
@@ -1,10 +1,24 @@
+const json2csv = require('json2csv').parse;
 const async = require('async');
 const express = require('express');
 const router = express.Router();
+const _ = require('underscore');
+const sequelize = require('../../models/index').sequelize;
+const Op = require('sequelize').Op;
 
-const models = require('../../models/index');
-const Report = models.Report;
-const ExpenseClaim = models.ExpenseClaim;
+const s3 = require('../../s3');
+
+const fs = require('fs');
+
+
+const database = require('../../models/index');
+const Report = database.Report;
+const Employee = database.Employee;
+const ExpenseClaim = database.ExpenseClaim;
+const ExpenseClaimItem = database.ExpenseClaimItem;
+const EmployeeExpenseClaim = database.EmployeeExpenseClaim;
+const CostCentre = database.CostCentre;
+
 
 /* GET /reports */
 router.get('', function (req, res, next) {
@@ -19,8 +33,26 @@ router.get('/statistics', function (req, res, next) {
 });
 router.get('/NAV', function (req, res, next) {
     res.locals.title = 'NAV Report';
+    Report.findAll({
+        where: {
+            type: {[Op.eq]: Report.TYPE.NAV}
+        }
+    }).then((reports) => {
+        var simplifiedReports = _.map(reports, (report) => {
+            var rep = {};
+            rep['id'] = report.id;
+            rep['type'] = report.type;
 
-    res.render('admin/NAVReport');
+            // TODO: need to add status to reports
+            // rep['status'] = report.status;
+
+            return rep;
+        });
+        res.locals.reports = simplifiedReports;
+
+        res.render('admin/NAVReport');
+    });
+
 });
 
 router.post('/statistic', function (req, res, next) {
@@ -31,52 +63,89 @@ router.post('/statistic', function (req, res, next) {
     })
 });
 router.post('/NAV', function (req, res, next) {
-    helper(req).then(function () {
-        res.redirect('/reports/NAV')
-    }).catch(function (error) {
-        next(error)
-    })
+    // TODO deal with non-statistics reports
+    console.log(req.body);
+    generateT24Report(req.body.report_start_date, req.body.report_end_date);
 });
-helper = function (req) {
-    return new Promise(function (fulfill, reject) {
-        // console.log(req.body);
-        let pArr = [];
-        let body = req.body;
-        let bodyArr = Object.keys(body);
-        for (let x of bodyArr) {
-            // console.log(body[x])
-        }
-        if (bodyArr.includes('allsub')) {
-            // console.log('All Submitters');
-            pArr.push(new Promise(function (fulfill) {
-                fulfill('All Submitters')
-            }))
-        }
-        if (bodyArr.includes('allapp')) {
-            // console.log('All Approvers');
-            pArr.push(new Promise(function (fulfill) {
-                fulfill('All Approvers')
-            }))
-        }
-        if (bodyArr.includes('allcc')) {
-            // console.log('All Cost Centres');
-            pArr.push(new Promise(function (fulfill) {
-                fulfill('All Cost Centres')
-            }))
-        }
-        if (bodyArr.includes('alld')) {
-            // console.log('All Date');
-            pArr.push(new Promise(function (fulfill) {
-                fulfill('All Date')
-            }))
-        }
 
-        Promise.all(pArr).then(function (results) {
-            fulfill()
-        }).catch(function (error) {
-            reject(error)
-        })
-    })
-};
+
+
+function generateNAVReport(startDate, endDate){
+
+}
+
+function generateT24Report(startDate, endDate){
+    async.waterfall([
+        function(callback){
+            ExpenseClaim.findAll({
+                where: {
+                    created_at: {[Op.gte]: startDate, [Op.lt]: endDate}
+                }, include: [{
+                    model: ExpenseClaimItem,
+                }, {
+                    model: EmployeeExpenseClaim,
+                    where: {
+                        isActive: {[Op.eq]: 1}
+                    }
+                }]
+            }).then(function(expenseClaims){
+                console.log(expenseClaims.length);
+                var rows = _.map(expenseClaims, (expenseClaim) => {
+                    var amount = _.reduce(expenseClaim.ExpenseClaimItem, (memo, expenseClaimItem) => {
+                        return memo + expenseClaimItem.total;
+                    }, 0);
+                    var row = {
+                        bank_number: expenseClaim.bankNumber,
+                        currency_type: "CAD",
+                        date: expenseClaim.createdAt,
+                        status: expenseClaim.status,
+                        dollar_amount: "$" + amount,
+                        _51: "51",
+                        CR: "CR",
+                        "-": "-",
+                        status: expenseClaim.status,
+                        empty1:"",
+                        empty2:"",
+                        empty3:"",
+                        dollar_value: amount,
+                        bank_number2: expenseClaim.bankNumber,
+                    };
+                    return row;
+                });
+
+                var opts = {};
+                opts['fields'] = rows[0].keys;
+                opts['delimiter'] = "|";
+                opts['header'] = false;
+                opts['quote'] = "";
+
+                var csv = json2csv(rows, opts);
+                console.log(csv);
+
+                var params = {
+                    Bucket: s3.config.params.Bucket,
+                    Key: "t24testreport",
+                    Body: csv
+                };
+                s3.putObject(params, function(err, data){
+                    if (err) {
+                        console.log(err)
+                    } else {
+                        console.log("Successfully uploaded data to myBucket/myKey");
+                        console.log("data returned:");
+                        console.log(data);
+                    }
+                });
+            });
+        }
+    ], function(err){
+        if(err){
+            console.log(err);
+            next(err);
+        } else {
+            res.redirect('/reports/NAV');
+        }
+    });
+}
 
 module.exports = router;
